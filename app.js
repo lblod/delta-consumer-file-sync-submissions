c287f7c192e97c4ef7dc38498ab2b7a925434c4d
felix <ruiz.felixruiz@gmail.com>
2021-12-07 12:47:16 +0100

    Added the deletion process to trigger file deletes

--- app.js
+++ app.js
@@ -2,25 +2,46 @@ import bodyParser from 'body-parser';
 import { CronJob } from 'cron';
 import { app, errorHandler } from 'mu';
 import {
-  CRON_PATTERN_HEALING_SYNC, DISABLE_AUTOMATIC_SYNC, SERVICE_NAME, DISABLE_INITIAL_SYNC
+    CRON_PATTERN_HEALING_SYNC, DISABLE_AUTOMATIC_SYNC, DISABLE_INITIAL_SYNC, SERVICE_NAME
 } from './config';
-import { getUnsyncedUris } from './lib/file-sync-job-utils';
-import { ProcessingQueue } from './lib/processing-queue';
-import { syncFilesAddition } from './pipelines/files-sync';
 import { waitForDatabase } from './lib/database';
+import { getUrisToDelete, getUrisToSync } from './lib/file-sync-job-utils';
+import { ProcessingQueue } from './lib/processing-queue';
+import { syncFile } from './pipelines/files-sync';
 
 const fileSyncQueue = new ProcessingQueue('file-sync-queue');
+
+/**
+ * BACKGROUND JOBS
+ **/
 
 console.log(`Status DISABLE_INITIAL_SYNC: ${DISABLE_INITIAL_SYNC}`);
 if(!DISABLE_INITIAL_SYNC){
   waitForDatabase(() => {
     fileSyncQueue.addJob(async () => {
-      const unsyncedFileUris = await getUnsyncedUris();
-      await syncFilesAddition(unsyncedFileUris.map(t => t.pFile));
+      await syncFile(await getUrisToDelete(), await getUrisToSync());
       console.log(`Initial sync was success`);
     });
   });
 }
+
+new CronJob(CRON_PATTERN_HEALING_SYNC, async function() {
+  if(DISABLE_AUTOMATIC_SYNC) {
+    console.log('Sync disabled, doing nothing');
+  }
+  else {
+    const now = new Date().toISOString();
+    console.info(`Delta healing sync triggered by cron job at ${now}`);
+    fileSyncQueue.addJob(async () => {
+      await syncFile(await getUrisToDelete(), await getUrisToSync());
+      console.log(`Healing sync was success`);
+    });
+  }
+}, null, true);
+
+/**
+ * API
+ **/
 
 app.use( bodyParser.json( { type: function(req) { return /^application\/json/.test( req.get('content-type') ); } } ) );
 
@@ -33,41 +54,25 @@ app.post('/delta', async function(req, res){
     console.log(`Status of DISABLE_AUTOMATIC_SYNC: ${DISABLE_AUTOMATIC_SYNC}`);
   }
   else {
-    const delta = req.body;
-    const inserts = delta.map(changeSet => changeSet.inserts).flat();
-    const subjects = [ ...new Set(inserts.map(t => t.subject.value)) ];
-
-    if(subjects.length){
+    const changeSets = req.body;
+    for(const changeSet of changeSets){
+      const deletes = [ ...new Set(changeSet.deletes(t => t.subject.value)) ];
+      const inserts = [ ...new Set(changeSet.inserts(t => t.subject.value)) ];
       fileSyncQueue.addJob(async () => {
-        await syncFilesAddition(subjects);
+        await syncFile(deletes, inserts);
       });
     }
   }
   res.status(202).send();
 });
 
-new CronJob(CRON_PATTERN_HEALING_SYNC, async function() {
-  if(DISABLE_AUTOMATIC_SYNC) {
-    console.log('Sync disabled, doing nothing');
-  }
-  else {
-    const now = new Date().toISOString();
-    console.info(`Delta healing sync triggered by cron job at ${now}`);
-    fileSyncQueue.addJob(async () => {
-      const unsyncedFileUris = await getUnsyncedUris();
-      await syncFilesAddition(unsyncedFileUris.map(t => t.pFile));
-    });
-  }
-}, null, true);
-
-/*
- * ENDPOINTS CURRENTLY MEANT FOR DEBUGGING
- */
+/**
+ * DEBUG-API
+ **/
 
 app.post('/file-sync-jobs', async function( _, res ){
   fileSyncQueue.addJob(async () => {
-    const unsyncedFileUris = await getUnsyncedUris();
-    await syncFilesAddition(unsyncedFileUris.map(t => t.pFile));
+    await syncFile(await getUrisToDelete(), await getUrisToSync());
   });
   res.send({ msg: 'Started file sync job' });
 });
